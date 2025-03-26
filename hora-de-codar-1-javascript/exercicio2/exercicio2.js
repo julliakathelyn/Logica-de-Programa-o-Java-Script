880e47ba2ae2938c03fb0e0633f9c35605d2c0c4
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2025-03-26 18:20:55 -0300

    3 exercícios js

---
+++ hora-de-codar-1-javascript/exercicio2/exercicio2.js
@@ -0,0 +1,8 @@
+// 2 Escreva um programa em que o usuário informe o seu nome e exiba a mensagem 
+// "Olá, [NomeDoUsuario]".
+
+var nomeDoUsuario
+
+nomeDoUsuario = prompt("Digite seu nome");
+
+alert("Ola " + nomeDoUsuario);
---
+++ hora-de-codar-1-javascript/exercicio3-retangulo/exercicio3.js
@@ -0,0 +1,10 @@
+//4 Considerando a figura abaixo, escreva um programa para cada forma que calcule e exiba em tela cada uma de suas respectivas áreas. O usuário irá informar os valores de cada variável.
+
+var base = 0;
+var altura = 0;
+var retangulo = base*altura
+
+base = prompt("Digite a base")
+altura = prompt("Digite a altura")
+
+console.log("A area do retangulo é: " + retangulo)
