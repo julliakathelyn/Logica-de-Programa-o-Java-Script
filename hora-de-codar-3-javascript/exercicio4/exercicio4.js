1e850326e60693fe1d05200a5247eb3ec9c062b1
JulliaKathelyn <137806375+julliakathelyn@users.noreply.github.com>
2025-04-07 12:36:03 -0300

    Hora de codar 3 De volta ao Código {Javascript}

---
+++ hora-de-codar-3-javascript/exercicio4/exercicio4.js
@@ -0,0 +1,8 @@
+//4 - Faça um algoritmo que calcule e escreva a média aritmética dos números inteiros entre 15 (inclusive) e 100 (inclusive).
+
+var n1 = 15;
+var n2 = 100;
+
+var media = (((n2 - n1) + 1) * (n2 + n1)) /2;
+
+alert(media);
